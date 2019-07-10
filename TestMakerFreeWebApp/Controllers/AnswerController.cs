a4a2faba75324890d0d9e68080edbd6404752221
Jamie Warren <jamie91warren@gmail.com>
2019-07-10 16:17:18 +0100

    Implemented CRUD operations in Answer, Question and Result controllers

--- TestMakerFreeWebApp/Controllers/AnswerController.cs
+++ TestMakerFreeWebApp/Controllers/AnswerController.cs
@@ -3,44 +3,37 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TestMakerFreeWebApp.ViewModels;
 using System.Collections.Generic;
+using TestMakerFreeWebApp.Data;
+using System.Linq;
+using Mapster;
 
 namespace TestMakerFreeWebApp.Controllers
 {
     [Route("api/[controller]")]
     public class AnswerController : Controller
     {
+        #region Private Fields
+        private ApplicationDbContext DbContext;
+        #endregion
+
+        #region Constructor
+        public AnswerController(ApplicationDbContext context)
+        {
+            // Instantiate the ApplicationDbContext through DI
+            DbContext = context;
+        }
+        #endregion
+
         // GET api/answer/all 
         [HttpGet("All/{questionId}")]
         public IActionResult All(int questionId)
         {
-            var sampleAnswers = new List<AnswerViewModel>();
+            var answers = DbContext.Answers
+                .Where(q => q.QuestionId == questionId)
+                .ToArray();
 
-            // add a first sample answer 
-            sampleAnswers.Add(new AnswerViewModel()
-            {
-                Id = 1,
-                QuestionId = questionId,
-                Text = "Friends and family",
-                CreatedDate = DateTime.Now,
-                LastModifiedDate = DateTime.Now
-            });
-
-            // add a bunch of other sample answers 
-            for (int i = 2; i <= 5; i++)
-            {
-                sampleAnswers.Add(new AnswerViewModel()
-                {
-                    Id = i,
-                    QuestionId = questionId,
-                    Text = String.Format("Sample Answer {0}", i),
-                    CreatedDate = DateTime.Now,
-                    LastModifiedDate = DateTime.Now
-                });
-            }
-
-            // output the result in JSON format 
             return new JsonResult(
-                sampleAnswers,
+                answers.Adapt<AnswerViewModel[]>(),
                 new JsonSerializerSettings()
                 {
                     Formatting = Formatting.Indented
@@ -56,27 +49,109 @@ namespace TestMakerFreeWebApp.Controllers
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            return Content("Not implemented (yet)!");
+            var answer = DbContext.Answers
+                .Where(i => i.Id == id)
+                .FirstOrDefault();
+
+            // handle requests asking for non-existing answers
+            if (answer == null)
+            {
+                return NotFound(new
+                {
+                    Error = String.Format("Answer ID {0} has not been found", id)
+                });
+            }
+
+            return new JsonResult(
+                answer.Adapt<AnswerViewModel>(),
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
         }
 
         /// <summary> 
         /// Adds a new Answer to the Database 
         /// </summary> 
-        /// <param name="m">The AnswerViewModel containing the data to insert</param> 
+        /// <param name="model">The AnswerViewModel containing the data to insert</param> 
         [HttpPut]
-        public IActionResult Put(AnswerViewModel m)
+        public IActionResult Put([FromBody]AnswerViewModel model)
         {
-            throw new NotImplementedException();
+            // return a generic HTTP Status 500 (Server Error)
+            // if the client payload is invalid.
+            if (model == null) return new StatusCodeResult(500);
+
+            // map the ViewModel to the Model
+            var answer = model.Adapt<Answer>();
+
+            // override those properties 
+            //   that should be set from the server-side only
+            answer.QuestionId = model.QuestionId;
+            answer.Text = model.Text;
+            answer.Notes = model.Notes;
+
+            // properties set from server-side
+            answer.CreatedDate = DateTime.Now;
+            answer.LastModifiedDate = answer.CreatedDate;
+
+            // add the new answer
+            DbContext.Answers.Add(answer);
+            // persist the changes into the Database.
+            DbContext.SaveChanges();
+
+            // return the newly-created Answer to the client.
+            return new JsonResult(answer.Adapt<AnswerViewModel>(),
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
         }
 
         /// <summary> 
         /// Edit the Answer with the given {id} 
         /// </summary> 
-        /// <param name="m">The AnswerViewModel containing the data to update</param> 
+        /// <param name="model">The AnswerViewModel containing the data to update</param> 
         [HttpPost]
-        public IActionResult Post(AnswerViewModel m)
+        public IActionResult Post([FromBody]AnswerViewModel model)
         {
-            throw new NotImplementedException();
+            // return a generic HTTP Status 500 (Server Error)
+            // if the client payload is invalid.
+            if (model == null) return new StatusCodeResult(500);
+
+            // retrieve the answer to edit
+            var answer = DbContext.Answers
+                .Where(q => q.Id == model.Id)
+                .FirstOrDefault();
+
+            // handle requests asking for non-existing answers
+            if (answer == null)
+            {
+                return NotFound(new
+                {
+                    Error = String.Format("Answer ID {0} has not been found", model.Id)
+                });
+            }
+
+            // handle the update (without object-mapping)
+            //   by manually assigning the properties 
+            //   we want to accept from the request
+            answer.QuestionId = model.QuestionId;
+            answer.Text = model.Text;
+            answer.Value = model.Value;
+            answer.Notes = model.Notes;
+
+            // properties set from server-side
+            answer.LastModifiedDate = answer.CreatedDate;
+
+            // persist the changes into the Database.
+            DbContext.SaveChanges();
+
+            // return the updated Quiz to the client.
+            return new JsonResult(answer.Adapt<AnswerViewModel>(),
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
         }
 
         /// <summary> 
@@ -86,7 +161,28 @@ namespace TestMakerFreeWebApp.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            throw new NotImplementedException();
+            // retrieve the answer from the Database
+            var answer = DbContext.Answers
+                .Where(i => i.Id == id)
+                .FirstOrDefault();
+
+            // handle requests asking for non-existing answers
+            if (answer == null)
+            {
+                return NotFound(new
+                {
+                    Error = String.Format("Answer ID {0} has not been found", id)
+                });
+            }
+
+            // remove the quiz from the DbContext.
+            DbContext.Answers.Remove(answer);
+            // persist the changes into the Database.
+            DbContext.SaveChanges();
+
+            // return an HTTP Status 200 (OK).
+            //return new OkResult();
+            return new NoContentResult();
         }
         #endregion
     }
